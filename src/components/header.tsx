26db9f1b37425c33dfce99606f42e65ec552d02e
Ivana Arbutina <ivana.arbutina2@gmail.com>
2023-03-29 16:22:30 +0200

    new-react

---
+++ src/components/header.tsx
@@ -0,0 +1,36 @@
+import logo from "./../assets/new-logo.svg"
+import hamburger from "./../assets/icon-hamburger.svg";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__inner">
+        <div>
+          <img className="header__logo" src={logo} alt="Logo" />
+        </div>
+        <div className="header__actions">
+          <nav className="header__nav">
+            <a className="header__nav__item" href="">
+              Home
+            </a>
+            <a className="header__nav__item" href="">
+              Blog
+            </a>
+            <a className="header__nav__item" href="">
+              About
+            </a>
+            <a className="header__nav__item" href="">
+              Contact
+            </a>
+          </nav>
+          <div className="header__actions__name">Ivana</div>
+        </div>
+        <div className="header__hamburger">
+          <img src={hamburger} alt="hamburger menu icon" />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
