487c9c13b8e60b78c4f56d241e88269868d5cd30
grafika10 <grafika10@ucione.local>
2023-03-30 20:24:26 +0200

    style

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,5 +1,19 @@
 const Modal = () => {
-  return <div className="modal">Text</div>;
+  return (
+    <div className="modal">
+      <div className="modal__box">
+        <h1>Modal Box</h1>
+        <p className="modal__box__message">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.<br></br>{" "}
+          Quos, laudantium necessitatibus maiores Itaque, consequatur error.
+        </p>
+        <div>
+          <button className="modal__box__btn">Open</button>
+          <button className="modal__box__btn2">Close</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
