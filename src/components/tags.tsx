154d48d3e948e6b2fc076ee4d270c4a12e47edd7
Ivana Arbutina <ivana.arbutina2@gmail.com>
2023-04-05 17:03:32 +0200

    add colors

--- src/components/tags.tsx
+++ src/components/tags.tsx
@@ -4,20 +4,17 @@ import sun from "./../assets/sun.svg";
 const Tags = () => {
   return <div className="tags">
     <div className="tags__section">
-    <img src={sadeFace} alt="sade-face" className="tags__section__img" />
-    <span ><p className="tags__section__name">Tag sade</p></span>
+      <img src={sadeFace} alt="sade-face" className="tags__section__img" />
+      <span ><p className="tags__section__name">Tag sade</p></span>
     </div>
     <div className="tags__section">
-    <img src={happyFace} alt="sade-face" className="tags__section__img" />
-    <span ><p className="tags__section__name">Tag happy</p></span>
+      <img src={happyFace} alt="sade-face" className="tags__section__img" />
+      <span ><p className="tags__section__name">Tag happy</p></span>
     </div>
     <div className="tags__section">
-    <img src={sun} alt="sade-face" className="tags__section__img" />
-    <span ><p className="tags__section__name">Tag shiny</p></span>
+      <img src={sun} alt="sade-face" className="tags__section__img" />
+      <span ><p className="tags__section__name">Tag shiny</p></span>
     </div>
-    
-    
-    
   </div>;
 };
 
