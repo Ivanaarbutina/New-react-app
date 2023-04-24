f83d8fc843ae5f91e0c5dac9cf3c62e745be2f3e
Ivana Arbutina <ivana.arbutina2@gmail.com>
2023-04-24 16:49:28 +0200

    add teorem list

--- src/pages/teorem.tsx
+++ src/pages/teorem.tsx
@@ -55,34 +55,34 @@ const Teorem = () => {
         </p>
         <div className="tutoring__card-wrapper">
           <div className="tutoring__card tutoring__card--1">
-            <img src={ComputerDyn} />
+            <img className="tutoring__img" src={ComputerDyn} />
             <h3>Fully remote</h3>
-            <p>All you need laptop and wifi.</p>
+            <p className="tutoring__text">All you need laptop and wifi.</p>
           </div>
           <div className="tutoring__card tutoring__card--2">
-            <img src={VideoCam} />
+            <img className="tutoring__img" src={VideoCam} />
             <h3>Tools you need</h3>
-            <p>Booking system, chat, video calls & more</p>
+            <p className="tutoring__text">Booking system, chat, video calls & more</p>
           </div>
           <div className="tutoring__card tutoring__card--3">
-            <img src={MoneyDyn} />
+            <img className="tutoring__img" src={MoneyDyn} />
             <h3>Secure Income</h3>
-            <p>We make sure you get paid.</p>
+            <p className="tutoring__text">We make sure you get paid.</p>
           </div>
           <div className="tutoring__card tutoring__card--4">
-            <img src={Dollar} />
+            <img className="tutoring__img" src={Dollar} />
             <h3>Set Your Price</h3>
-            <p>No limitations what you can earn.</p>
+            <p className="tutoring__text">No limitations what you can earn.</p>
           </div>
           <div className="tutoring__card tutoring__card--5">
-            <img src={ChatBubble} />
+            <img className="tutoring__img" src={ChatBubble} />
             <h3>24/7 Support</h3>
-            <p>We make sure all your issues are solved.</p>
+            <p className="tutoring__text">We make sure all your issues are solved.</p>
           </div>
           <div className="tutoring__card tutoring__card--6">
-            <img src={ChartDin} />
+            <img className="tutoring__img" src={ChartDin} />
             <h3>Improve Your Teaching</h3>
-            <p>We take care of administration, you just focus the lessons.</p>
+            <p className="tutoring__text">We take care of administration, you just focus the lessons.</p>
           </div>
         </div>
 
@@ -90,11 +90,73 @@ const Teorem = () => {
           <p className="teorem__tutor__info">
             "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque ex
             accusantium fuga similique, quisquam quod placeat? Nihil facere
-            maiores voluptatibus aspernatur impedit inventore omnis eos, beatae
-            ipsa minus repellendus voluptates."
+            maiores voluptatibus."
           </p>
-          <img className="teorem__tutor__pic" src={John} />
+          <div>
+            <img className="teorem__tutor__pic" src={John} />
+            <h4>John Doe</h4>
+            <span className="teorem__tutor__name">Tutor</span>
+          </div>
         </div>
+
+        
+        <div>   
+          <h2 className="tutoring__title">
+            Set your own price and availability
+          </h2>
+          <p className="tutoring__desc">
+            Use our tool to track how much you're<br></br>
+            making and incerase your client base!
+          </p>
+          <img className="teorem__section__planer" src={TeoremImg} />
+          <h2 className="tutoring__title">
+            We are here to help you grow!
+          </h2>
+        </div>
+        <div className="teorem__tutor">
+          <p className="teorem__tutor__info">
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque ex
+            accusantium fuga similique, quisquam quod placeat? Nihil facere
+            maiores voluptatibus."
+          </p>
+          <div>
+            <img className="teorem__tutor__pic" src={John} />
+            <h4>John Doe</h4>
+            <span className="teorem__tutor__name">Tutor</span>
+          </div>
+        
+      </div>
+      <div>
+         <h2 className="tutoring__title">
+            How to become an online<br></br> tutor on Teorem
+         </h2>
+         <div className="tutoring__list">
+            <div className="tutoring__list__numb">1</div>
+            <span className="tutoring__list__text">Create your acount and fill in the information</span>
+         </div>
+         <div className="tutoring__list__line"></div>
+         <div className="tutoring__list">
+            <div className="tutoring__list__numb">2</div>
+            <span>Upload a short video to prove your identity</span>
+         </div>
+         <hr className="tutoring__list__line2"></hr>
+         <div className="tutoring__list">
+            <div className="tutoring__list__numb">3</div>
+            <span>Complete your profile and start tutoring</span>
+         </div>
+         <button className="tutoring__list__btn">Get started</button>
+         <h2 className="tutoring__title">
+            Frequently asked quesions
+         </h2>
+         <div>
+            <div><p>What are the requirements for tutors?</p><span></span></div>
+            <div><p>How long it takes before I can start teaching?</p><span></span></div>
+            <div><p>How does the subscription work?</p><span></span></div>
+            <div><p>What does the employment relationship look like?</p></div>
+            <div><p>What makes Teorem differenu from other platforms?</p></div>
+         </div>
+      </div>
+       
       </div>
     </div>
   );
