e5114576f44d6ce432a7b59cc83173d528ee793e
Ivana Arbutina <ivana.arbutina2@gmail.com>
2023-04-28 14:12:55 +0200

    add  exercises to vjezba2604

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,17 @@
 const Home = () => {
-  return <div className="container">Home</div>;
+  return <div className="container">
+    <h2>Home</h2>
+    <div>
+        <h3>Vježbe</h3>
+        <ul>
+          <li>
+            <a href="/petlje">Petlje</a>
+          </li>
+          <li>
+            <a href="/vjezba2604">Vjezba 2604</a>
+          </li>
+        </ul>
+      </div>
+  </div>;
 };
 export default Home;
--- src/pages/vjezba2604.tsx
+++ src/pages/vjezba2604.tsx
@@ -1,25 +1,33 @@
 const Vjezba2604 = () => {
+  //Nizovi
   let arr = ["mama", "tata", "baka", "dida"];
-
   for (let i = 0; i < arr.length; i++) {
-    //if (i == 2) {
-    //  break;
-    //}
-    // console.log(arr[i]);
+    //kada dodjemo ddo vrijednosti "baka" izadji iz for petlje
+    if(arr[i]=="baka") {
+      break;
+    }
+    //izbaci vrijednost "tata" iz array-a
+    if (arr[i]=="tata") {
+      delete arr[i];
+      //ili
+      const removeIndex = arr.indexOf("tata");
+      //koristi splice samo kada je index pronadjen što zneči da mora biti veći od -1
+      if (removeIndex > -1){
+        arr.splice(removeIndex, 1);
+      }
+    }
+    console.log(arr[i]);
   }
-  // arr.push("Ivana");
-
-  // console.log("nas array je:", arr);
+  delete arr[1];
+   arr.push("Ivana");
+   console.log("nas array je: ", arr);
 
   //Funkcije
-
   let baka: string;
-
   function bringBaka() {
     for (let i = 0; i < arr.length; i++) {
       if (arr[i] == "baka") {
         baka = arr[i];
-        console.log(baka);
         return baka;
       }
     }
@@ -60,21 +68,72 @@ const Vjezba2604 = () => {
   //Zadatak
 
   const brojevi = [1, 3, 5, 8, 9, 10, 15, 16, 18];
-  const greatestNumber:number;
-  function biggestNumber() {
+  let largest = Math.max(1, 3, 5, 8, 9, 10, 15, 16, 18);
+  function biggestNumber () {
+    let greatestNumber:number= 0;
     for (let i = 0; i < brojevi.length; i++) {
-      if (brojevi[i]) {
-      
+      if (brojevi[i]> greatestNumber) {
+        greatestNumber = brojevi[i];
       }
     }
+    return greatestNumber;
+  }
+ 
+  
+  //Zadača
+  //ispiši najdužu i najkraću riječ iz niza
+
+  
+  const rijeci = [
+    "balance",
+    "biscuit",
+    "side",
+    "grind",
+    "refund",
+    "cast",
+    "circulate",
+    "accumulation",
+    "fold",
+    "cut",
+    "entertainment",
+  ];
+  function findLongestWord (){
+   let longestWord = 0;
+   for (let i=0; i < rijeci.length; i++){
+    if(rijeci[i].length > longestWord){
+      longestWord == rijeci[i].length;
+    }
+   }
+    return longestWord;
+  };
+
+
+  function findShortestWord (){
+    let shortestWord = 0;
+    for (let i=0; i > rijeci.length; i++){
+      if(rijeci[i].length < shortestWord){
+        shortestWord == rijeci[i].length;
+      }
+    }
+    return shortestWord;
+  };
+
 
   return (
-    <div>
+    <div className="container">
       <h1>Funkcije</h1>
-      {bringBaka()}
-
+      <div>{bringBaka()}</div>
+      <div>{printText("Neki tekst")}</div>
+      <h1>Objekti</h1>
+      <div>{recenica}</div>
+      <pre>{JSON.stringify(car, null, 2)}</pre>
       <div>{printValue("motor")}</div>
+      <div>{largest}</div>
+      <div>{greatestNumber}</div>
+      <div>{longestWord}</div>
+      <div>{shortestWord}</div>
     </div>
   );
 };
+
 export default Vjezba2604;
