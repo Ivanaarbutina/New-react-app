4bb56c7f8d41fe59f25dbb7bab3c6964cc356b4c
grafika10 <grafika10@ucione.local>
2023-05-22 20:17:04 +0200

    add o

--- src/pages/zadaca.tsx
+++ src/pages/zadaca.tsx
@@ -13,7 +13,7 @@ const Zadaca = () => {
   if (myAge < 40) {
     console.log("nemam vise od 40 godina");
   } else if (myAge > 40) {
-    console.log("imam vise od 40 goddddina");
+    console.log("imam vise od 40 gooooddddina");
   }
 
   const test = 3;
