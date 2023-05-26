3872f411f0f67ba25a424841f2b0e33eb1dc9d99
Ivana Arbutina <ivana.arbutina2@gmail.com>
2023-05-26 17:55:37 +0200

    add vjezba

--- src/pages/vjezba12.05.tsx
+++ src/pages/vjezba12.05.tsx
@@ -1,8 +1,26 @@
 const Vjezba1205 = () => {
-  return (
+
+  //ukoliko zelimo selektirati sve elemente (button) onda to radimo na ovaj nacin
+  const klikni = (dugme: React.MouseEvent<HTMLButtonElement>) =>{
+    console.log(dugme.currentTarget.innerText);
+  }
+
+  let btns = document.querySelectorAll('button');
+  
+  btns.forEach(function (btn){
+    // Event listener koji ceka na klik, za svaki button
+    btn?.addEventListener('click', function () {
+      console.log('KLiknuo');
+    });
+  });
+
+  
+  return(
     <div>
-      <h2>Chat</h2>
+      <button className="btn"></button>
+      <button className="btn"></button>
+    
     </div>
-  );
+  )
 };
 export default Vjezba1205;
