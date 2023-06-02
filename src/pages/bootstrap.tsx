b1a8744cb1c6c8ef7851b0e775509541f0c75a2b
Ivana Arbutina <ivana.arbutina2@gmail.com>
2023-06-02 17:15:56 +0200

    add style to bootstrap

--- src/pages/bootstrap.tsx
+++ src/pages/bootstrap.tsx
@@ -4,16 +4,16 @@ import {
   Button,
   Alert,
   Card,
-  Spinner,
   Table,
   Tabs,
   Tab,
   TabPane,
   TabContainer,
+  ProgressBar,
 } from "react-bootstrap";
 import { famArt } from "../data/artData";
 import { useState } from "react";
-import frontRazred from "../pages/vjezba2804";
+import {frontRazred} from "../data/frontRazred";
 import { LineChart, XAxis, YAxis, CartesianGrid, Line } from "recharts";
 
 const data = [
@@ -61,11 +61,9 @@ type StudentType = {
 const BootstrapExe = () => {
   const [isBiografyVisible, setIsBiografyVisible] = useState(false);
   const [artistBiografy, setArtistBiografy] = useState<ArtType | null>(null);
-  const [show, setShow] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
   const handleClick = () => {
-    setShow(true);
     setShowAlert(true);
   };
 
@@ -100,89 +98,96 @@ const BootstrapExe = () => {
 
       <div className="container">
         <div>
+
+        </div>
+        <div>
           <Button onClick={handleClick}>Click me</Button>
           {showAlert && (
             <Alert
               variant="danger"
               onClose={() => setShowAlert(false)}
               dismissible
+              className="bootstrap__alert"
             >
               <Alert.Heading>Kliknuo si me!</Alert.Heading>
-              <p>Hello Bootstrap</p>
             </Alert>
           )}
         </div>
-        <div></div>
+        
       </div>
       <div className="container">
-        <div>
-          <TabContainer defaultActiveKey="dashboard">
-            <Tab.Content>
-              <TabPane eventKey="dashboard" title="Dashboard">
-                <div></div>
-              </TabPane>
-            </Tab.Content>
-          </TabContainer>
+       <div>
+          <div >
+              <TabContainer defaultActiveKey="dashboard">
+                <Tab.Content>
+                  <TabPane eventKey="dashboard" title="Dashboard">
+                    <div></div>
+                  </TabPane>
+                </Tab.Content>
+              </TabContainer>
 
-          <Tabs
-            defaultActiveKey="profile"
-            id="justify-tab-example"
-            className="mb-3"
-            justify
-          >
-            <Tab eventKey="dashboard" title="Dashboard">
-              <div className="container__section">
-                <Card className="container__card">
-                  <Card.Body>
-                    <div className="container__section">
-                      <span className="container__numb">45</span>
-                      <div>
-                        <p>Ovo je neki text</p>
-                        <p>koji nesto objasnjava</p>
-                      </div>
-                    </div>
-                  </Card.Body>
-                </Card>
-                <Card className="container__card">
-                  <LineChart width={500} height={300} data={data}>
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-                    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-                    <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
-                  </LineChart>
-                </Card>
-              </div>
+              <Tabs
+                defaultActiveKey="profile"
+                id="justify-tab-example"
+                className="mb-3"
+                justify
+              >
+                <Tab eventKey="dashboard" title="Dashboard">
+                  <div className="bootstrap__section">
+                    <Card className="bootstrap__cards">
+                      <Card.Body>
+                        <div className="bootstrap__boxs" >
+                          <span className="bootstrap__numb">45</span>
+                          <div >
+                            <p>Ovo je neki text</p>
+                            <p>koji nesto objasnjava</p>
+                          </div>
+                        </div>
+                        <ProgressBar striped variant="info" now={20} />
+                      </Card.Body>
+                    </Card>
+                    <Card className="bootstrap__cards">
+                      <LineChart width={393} height={180} data={data}>
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+                        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                        <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
+                      </LineChart>
+                    </Card>
+                  </div>
 
-              <Table className="varijable__table">
-                <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>LastName</th>
-                    <th>Height</th>
-                    <th>Hobby</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {/* {frontRazred.map((student: StudentType) => {
-                    return (
-                      <tr key={student.name}>
-                        <td>{student.name}</td>
-                        <td>{student.lastName}</td>
-                        <td>{student.height}</td>
-                        <td>{student.hobby}</td>
+                  <Table >
+                    <thead>
+                      <tr>
+                        <th>Name</th>
+                        <th>LastName</th>
+                        <th>Height</th>
+                        <th>Hobby</th>
                       </tr>
-                    );
-                  })} */}
-                </tbody>
-              </Table>
-            </Tab>
-            <Tab eventKey="game" title="Game">
-              <Spinner animation="border" />
-            </Tab>
-          </Tabs>
+                    </thead>
+                    <tbody>
+                      {frontRazred.map((student: StudentType) => {
+                        return (
+                          <tr key={student.name}>
+                            <td>{student.name}</td>
+                            <td>{student.lastName}</td>
+                            <td>{student.height}</td>
+                            <td>{student.hobby}</td>
+                          </tr>
+                        );
+                      })} 
+                    </tbody>
+                  </Table>
+                </Tab>
+                <Tab eventKey="game" title="Game">
+                  
+                </Tab>
+              </Tabs>
+            </div>
+          </div>
         </div>
-      </div>
+      
     </div>
   );
 };
